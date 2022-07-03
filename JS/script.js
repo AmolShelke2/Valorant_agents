e6525800853e7953485303245ef9569d63d429fd
Amol shelke <shelkeamol897@gmail.com>
2022-07-03 17:40:10 +0530

    update index.html

--- JS/script.js
+++
@@ -1,7 +0,0 @@
-const menu = document.getElementById("menu");
-const menuItem = document.querySelector(".menu-list");
-
-menu.addEventListener("click", function () {
-  console.log("menu clicked");
-  menuItem.classList.toggle("active");
-});
